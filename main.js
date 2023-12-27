9488a9e1f4cc7311efe37841780ad3e8353c15a8
Up Crown <wangshangguan@hotmail.com>
2023-12-27 15:38:52 -0800

    create input for seed color [Issue #1]

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Color Scheme Generator</h1>
+    <div>
+      <input id="seed-color" name="seed-color" type="color"/>
+    </div>
   </div>
 `
 
