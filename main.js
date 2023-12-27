397f737a3524ac7ad890cbe183af2f55a9e5e695
Up Crown <wangshangguan@hotmail.com>
2023-12-27 15:45:37 -0800

    create scheme mode select box [Issue #2]

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ document.querySelector('#app').innerHTML = `
     <h1>Color Scheme Generator</h1>
     <div>
       <input id="seed-color" name="seed-color" type="color"/>
+      <select id="scheme-mode" name="scheme-mode">
+        <option value="monochrome">monochrome</option>
+        <option value="monochrome-dark">monochrome-dark</option>
+        <option value="monochrome-light">monochrome-light</option>
+        <option value="analogic">analogic</option>
+        <option value="complement">complement</option>
+        <option value="analogic-complement">analogic-complement</option>
+        <option value="triad">triad</option>
+        <option value="quad">quad</option>
+      </select>
     </div>
   </div>
 `
