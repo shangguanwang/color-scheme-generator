ce3e453424858c1f5731e0ea077b1a188518f8aa
Up Crown <wangshangguan@hotmail.com>
2023-12-27 17:30:14 -0800

    add CSS styling [Issue #4]

--- main.js
+++ main.js
@@ -20,16 +20,18 @@ let colorDisplayElement = document.getElementById("color-display");
 // Function to render colors from API
 const renderColors = (link, hexval) => {
   colorDisplayElement.innerHTML += `
+  <article class="individual-color">
   <img src="${link}" alt="color display for ${hexval} " >
   <p>${hexval}</p>
+  </article>
   `
 }
 
 // make an API request when user clicks the button
 document.getElementById("color-form").addEventListener("submit", (e)=>{
   e.preventDefault(); //prevent page from reloading
+  colorDisplayElement.innerHTML = ""; //reset color display 
   let URL = `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}&count=6`;
-  console.log(selectedColor, selectedMode);
   fetch(URL)
   .then(res=>res.json())
   .then(data=>{
