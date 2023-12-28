4d139858da8b8f7724322c4c4057c068bb335589
Up Crown <wangshangguan@hotmail.com>
2023-12-27 16:21:43 -0800

    make GET request and console log data [Issue #3]

--- main.js
+++ main.js
@@ -9,8 +9,18 @@ let selectedMode = schemeMode.value;
 
 seedColor.addEventListener("change", ()=>{
   selectedColor = seedColor.value;
+  selectedColor = selectedColor.slice(1,); // remove the # sign
 })
 schemeMode.addEventListener("change", ()=>{
   selectedMode = schemeMode.value;
 })
 
+// make an API request when user clicks the button
+document.getElementById("color-form").addEventListener("submit", (e)=>{
+  e.preventDefault(); //prevent page from reloading
+  let URL = `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}&count=6`;
+  console.log(selectedColor, selectedMode);
+  fetch(URL)
+  .then(res=>res.json())
+  .then(data=>console.log(data));
+})
