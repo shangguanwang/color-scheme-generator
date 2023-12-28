566446265f5a598051bc922afdb898cd6e954d5d
Up Crown <wangshangguan@hotmail.com>
2023-12-27 16:10:20 -0800

    get value when user changes input [Issue #3]

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Color Scheme Generator</h1>
-    <div>
-      <input id="seed-color" name="seed-color" type="color"/>
-      <select id="scheme-mode" name="scheme-mode">
-        <option value="monochrome">monochrome</option>
-        <option value="monochrome-dark">monochrome-dark</option>
-        <option value="monochrome-light">monochrome-light</option>
-        <option value="analogic">analogic</option>
-        <option value="complement">complement</option>
-        <option value="analogic-complement">analogic-complement</option>
-        <option value="triad">triad</option>
-        <option value="quad">quad</option>
-      </select>
-    </div>
-  </div>
-`
+// Get the updated value as the user changes selection
+let seedColor = document.getElementById("seed-color");
+let schemeMode = document.getElementById("scheme-mode");
+
+let selectedColor = seedColor.value;
+let selectedMode = schemeMode.value;
+
+seedColor.addEventListener("change", ()=>{
+  selectedColor = seedColor.value;
+})
+schemeMode.addEventListener("change", ()=>{
+  selectedMode = schemeMode.value;
+})
 
